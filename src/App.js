09de1f639c0b1a05414a43162634770293119944
rajalakshmi0811 <55794997+rajalakshmi0811@users.noreply.github.com>
2020-02-07 14:49:37 +0530

    add API calls

--- src/App.js
+++ src/App.js
@@ -1,62 +1,103 @@
 import React, { Component } from 'react';
-import {
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Button,
-  Container
-} from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button, Container } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navb from './nav';
 
-class App extends React.Component {
+class Home extends React.Component {
   state = {
-    result:0,
-    operand1:0,
-    operand2:0,
-    operator:'add'
+    result: 0,
+    operand1: 0,
+    operand2: 0,
+    operator: 'add'
   }
+
   onChangeInput = (e) => {
     const name = e.target.name;
+
     this.setState({
-        [name]: e.target.value
-    });
-}
-onChangeInput = (e) => {
-  const name = e.target.name;
-  this.setState({
       [name]: e.target.value
-  });
-}
+    });
+  }
+
+  submitButton = (e) => {
+    const { operand1, operand2, operator } = this.state;
+
+    let result;
+
+    if (operator === 'add') {
+      result = parseInt(operand1) + parseInt(operand2);
+    }
+
+    if (operator === 'sub') {
+      result = parseInt(operand1) - parseInt(operand2);
+    }
+
+    if (operator === 'multi') {
+      result = parseInt(operand1) * parseInt(operand2);
+    }
+
+    if (operator === 'div') {
+      result = parseInt(operand1) / parseInt(operand2);
+    }
+
+
+    this.setState({
+      result: result
+    });
+
+    fetch('http://localhost:9001/calculate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "operand1": Number(operand1),
+        "operand2": Number(operand2),
+        "operator": operator
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          result: data.result
+        });
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
   render = () => {
     return (
       <>
-        <Form>
-          <FormGroup>
-            <Label for="operand1">Operand 1</Label>
-            <Input type="number" name="operand1" id="operand1" placeholder="with a placeholder" onChange={this.onChangeInput} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="operand2">Operand 2</Label>
-            <Input type="number" name="operand2" id="operand2" placeholder="password placeholder" onChange={this.onChangeInput} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="operator">Operator</Label>
-            <Input type="select" name="operator" id="operator" onChange={this.onChangeInput} >
-              <option value="add">+</option>
-              <option value="sub">-</option>
-              <option value="multi">*</option>
-              <option value="div">/</option>
-            </Input>
-          </FormGroup>
-          <Button color="primary" type="button" >Submit</Button>{' '}
-          <Button color="danger" type="button">Cancel</Button>
-          <div>{this.state.result}</div>
-        </Form>
-
-
+        <Navb />
+        <div style={{ "margin": "auto", "width": "50%" }}>
+          <Form>
+            <FormGroup>
+              <Label for="operand1">Operand 1</Label>
+              <Input type="text" name="operand1" placeholder="number1" onChange={this.onChangeInput} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="operand2">Operand 2</Label>
+              <Input type="text" name="operand2" placeholder="Number2" onChange={this.onChangeInput} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="operator">Operator</Label>
+              <Input type="select" name="operator" onChange={this.onChangeInput} >
+                <option value="add">+</option>
+                <option value="sub">-</option>
+                <option value="multi">*</option>
+                <option value="div">/</option>
+              </Input>
+            </FormGroup>
+            <Button color="primary" type="button" onClick={this.submitButton} >Submit</Button>{' '}
+            <Button color="danger" type="button">Cancel</Button>
+            <div>{this.state.result}</div>
+          </Form>
+        </div>
       </>
     );
   }
 }
-export default App;
+export default Home;
 
